be608fd593a8a1149a8a25d33e09e143af720513
junwo <loust3598@naver.com>
2021-11-10 17:22:32 +0900

    Feat:todo 삭제

--- board/urls.py
+++ board/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BoardListView
+from .views import BoardListView, delete_board
 
 app_name = 'boards'
 
 urlpatterns = [
     path('', BoardListView.as_view(), name='board-list'),
+    path('<int:board_pk>/delete/', delete_board, name="delete-board",)
 ]
--- board/views.py
+++ board/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, reverse
 from django.views.generic import ListView
 from .models import Board
 
 
 class BoardListView(ListView):
-    model = Board
 
     context_object_name = "boards"
     paginate_by = 10
+
+    def get_queryset(self):
+        return Board.objects.order_by("-created", "tag", "content")
+
+
+def delete_board(self, board_pk=None):
+    print("="*50)
+    print(board_pk)
+    board = Board.objects.get(pk=board_pk)
+
+    board.delete()
+    return redirect(reverse("boards:board-list"))
