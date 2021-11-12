bd32b637b670b5f39440508643bf6942d335f81a
Jun woo <loust3598@naver.com>
2021-11-13 02:44:03 +0900

    Feat: todo 생성 페이지 태그 삭제

--- board/urls.py
+++ board/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BoardListView, BoardCreateView, delete_board, edit_board
+from .views import BoardListView, BoardCreateView, delete_board, delete_tag, edit_board, edit_tag, delete_tag
 
 app_name = 'boards'
 
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', BoardListView.as_view(), name='board-list'),
     path('<int:board_pk>/delete/', delete_board, name="delete-board",),
     path('edit/', edit_board, name="edit-board",),
+    path('edit-tag/', edit_tag, name="edit-tag",),
+    path('delete-tag/', delete_tag, name="delete-tag",),
     path('create/', BoardCreateView.as_view(), name="create-board",),
 ]
--- static/js/tagChoice.js
+++ static/js/tagChoice.js
@@ -1,8 +1,10 @@
-function tagChoice(id) {    
-    const tagColor = document.getElementById(`hidden-${id}`);
+function tagChoice(id) {   
+    const name = id.substring(5)
+    
+    const tagColor = document.getElementById(`hidden-${name}`);
     const inputColor = document.getElementById('input-color');
     const inputTag= document.getElementById('input-tag');
         
-    inputTag.value = id;
+    inputTag.value = name;
     inputColor.value= tagColor.value  == 'None' ?  '#FFFFFF' : tagColor.value ;
 }
